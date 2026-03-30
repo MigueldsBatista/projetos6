0815be3a892c3713d0b8c3d74036f3cab84be26e
Miguel Batista <msb2@cesar.school>
2026-03-29 22:41:20 -0300

    fix: correct model registration order in admin.py

--- apps/api/core/admin.py
+++ apps/api/core/admin.py
@@ -2,12 +2,14 @@
 
 from django.contrib import admin
 
+from core.models.analise import Analise
 from core.models.movimento import Movimento
 from core.models.orgao_julgador import OrgaoJulgador
 from core.models.palavra_chave import PalavraChave
 from core.models.processo import Processo
 
-admin.site.register(Processo)
+admin.site.register(Analise)
+admin.site.register(Movimento)
 admin.site.register(OrgaoJulgador)
 admin.site.register(PalavraChave)
-admin.site.register(Movimento)
+admin.site.register(Processo)
